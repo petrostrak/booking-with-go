768de3c7a72a9e2951a4b9d816f8f0f8c174dc97
petros <pit.trak@gmail.com>
2021-03-10 20:02:42 +0200

    Developing our own middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,7 +12,9 @@ import (
 // Routes func
 func routes(appC *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/petrostrak/booking-with-go
 
 go 1.16
 
-require github.com/go-chi/chi/v5 v5.0.1
+require (
+	github.com/go-chi/chi/v5 v5.0.1
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
