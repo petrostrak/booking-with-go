787919126c74306334b276bce9afd99e291e4279
petros <pit.trak@gmail.com>
2021-03-10 19:51:27 +0200

    Using chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,17 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/petrostrak/booking-with-go/pkg/config"
 	"github.com/petrostrak/booking-with-go/pkg/handlers"
 )
 
 // Routes func
 func routes(appC *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
