b859ac1a797d64892872fea57c730edff2bd0be6
petros <pit.trak@gmail.com>
2021-03-17 15:09:18 +0200

    Listing all reservations

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -416,8 +416,17 @@ func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request
 }
 
 func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request) {
+	reservations, err := m.DB.AllReservations()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["reservations"] = reservations
+
 	render.Template(w, r, "admin-all-reservations.page.tmpl", &models.TemplateData{
-		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
